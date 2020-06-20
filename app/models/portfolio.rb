1df28d689c844e3031610c31ac293fc74b1efc73
ribrciti <rbciti@msn.com>
2020-06-20 13:29:50 -0700

    Added image defaults for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -9,4 +9,16 @@ class Portfolio < ApplicationRecord
   #scope :angular, -> { where(subtitle: 'Angular') }
 
   scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby_on_Rails') }
+
+  
+#sets default values for images when portfolio controller actions runs so images
+#are not nil if not set for new images.
+  after_initialize :set_defaults  
+
+  def set_defaults
+    self.main_image   ||= "https://via.placeholder.com/600x400"
+    self.thumb_image  ||= "https://via.placeholder.com/350x200" 
+  end
+
+
 end
