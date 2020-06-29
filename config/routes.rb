a53f8cb45a256cd7c01acb609824735b05bd756a
ribrciti <rbciti@msn.com>
2020-06-29 10:12:25 -0700

    Added login and logout functions

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
       get :toggle_status      
     end    
   end
-  devise_for :users
+  devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   get 'home/index'
     mount Sidekiq::Web => '/sidekiq'
 
