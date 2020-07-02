d37fda3616da974ff66a4b7a9b3d09e3f6658ce1
ribrciti <rbciti@msn.com>
2020-07-02 15:04:30 -0700

    Added firstname lastname feature to user_name in User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+ 
+  validates_presence_of :user_name
+
+  def firstname 
+    self.user_name.split.first
+  end
+
+  def lastname
+    self.user_name.split.last
+  end
 end
