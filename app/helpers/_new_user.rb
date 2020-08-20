64718a1daed4faed873b26673691a108a20fac54
ribrciti <rbciti@msn.com>
2020-08-20 12:32:25 -0700

    Added view helper module for authorization

---
+++ app/helpers/_new_user.rb
@@ -0,0 +1,2 @@
+link_to "Register", new_user_registration_path, class: "btn btn-sm btn-primary"
+link_to "Sign_in", new_user_session_path, class: "btn btn-sm btn-primary"
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+  def login_helper
+    if current_user.is_a?(User)
+      logout
+    else
+      sign_in
+    end
+  end
+
+private
+
+def logout
+  link_to("Logout", destroy_user_session_path, method: :delete, data: { confirm: 'Are you sure?' }, class: "btn btn-sm btn-primary").html_safe
+end
+
+  def sign_in
+    return  link_to("Register", new_user_registration_path, class: "btn btn-sm btn-primary").html_safe + " " +
+            link_to("Sign_in", new_user_session_path, class: "btn btn-sm btn-primary").html_safe
+  end
 end
