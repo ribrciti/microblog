d9ae446bd1a0841d78277ec0dcf5e8657ba5a322
ribrciti <rbciti@msn.com>
2020-08-28 14:51:47 -0700

    Added a copyright feature and logo to layouts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,20 @@ class ApplicationController < ActionController::Base
   include CurrentUserConcern
   include SetTitlePageConcern
 
+  before_action :set_copyright
 
+  def set_copyright
+    @copyright = RRViewTool::Renderer.copyright 'Rick Richards', '  All rights reserved'
+  end
+end
+
+
+  module RRViewTool
+    class Renderer
+      def self.copyright name, msg
+        "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+      end
+
+
+  end
 end
